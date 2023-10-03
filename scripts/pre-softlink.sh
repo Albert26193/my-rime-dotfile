35f919769fd8782e1bb098aa6dde4e25649ced0e
Albert <albert990624@gmail.com>
2023-10-03 16:02:45 +0800

    feat(scripts): change pre-softlink

--- scripts/pre-softlink.sh
+++ scripts/pre-softlink.sh
@@ -1,7 +1,16 @@
 function link_to_rime {
 	local target_path="${HOME}/Library/Rime"
 	local git_root="$(git rev-parse --show-toplevel 2>/dev/null)"
-	ln -s "${git_root}" "${target_path}"
+
+	if [[ -d ${target_path} ]]; then
+		echo "${target_path} exist. back up it. exit..."
+	elif [[ ! -z "$(ls -A ${target_path})" ]]; then
+		echo "please back up origin file. exit..."
+	else
+		ln -s "${git_root}/config" "${target_path}"
+		echo "Link it successful."
+	fi
+
 }
 
 link_to_rime
