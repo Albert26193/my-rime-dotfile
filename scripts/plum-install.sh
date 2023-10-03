4623c3ca36d8af9c480e5f413b143081ecff8078
Albert <albert990624@gmail.com>
2023-10-03 15:41:31 +0800

    feat(double_pinyin_flypy): change it

--- scripts/plum-install.sh
+++ scripts/plum-install.sh
@@ -1,1 +1,7 @@
-echo ""
+function select_rime_install {
+	local git_root=$(git rev-parse --show-toplevel 2>/dev/null)
+	local target_path="${git_root}/plum"
+	bash -c "${target_path}/rime-install --select :all"
+}
+
+select_rime_install
--- scripts/pre-softlink.sh
+++ scripts/pre-softlink.sh
@@ -1,1 +1,7 @@
-echo ""
+function link_to_rime {
+	local target_path="${HOME}/Library/Rime"
+	local git_root="$(git rev-parse --show-toplevel 2>/dev/null)"
+	ln -s "${git_root}" "${target_path}"
+}
+
+link_to_rime
